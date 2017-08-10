afe05b7d3ec07a16e7f3828df650c4c78e8f7009
Erik Vorkink <erik@vorkink.com>
2017-08-10 13:41:07 -0600

    Added vuetify + navigation drawer. Added favicon. Mostly fixed logo transparency issues.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <div id="app">
-      <router-link to="/home"><img id="logo" src="./assets/logo.png" /></router-link>
-      <p>
-        <router-link to="/record">Record Outfit</router-link>
-        <router-link to="/history">History</router-link>
-      </p>
-      <router-view></router-view>
-  </div>
+  <v-app id="app" standalone>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      light
+      overflow
+      absolute
+    >
+      <v-list class="pa-0">
+        <v-list-tile>
+          <v-list-tile-content>
+            <img id="logo" src="./assets/logo.png" />
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon dark @click.native.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0">
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" router v-bind:to="item.href">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed dark>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>looking sharp, sir</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </main>
+  </v-app>
 </template>
 
 <script>
+import Record from '@/components/Record'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      drawer: true,
+      items: [
+        { title: 'Record Outfit', href: '/record', component: Record, icon: 'accessibility' },
+        { title: 'History', href: '/history', icon: 'history' }
+      ],
+      mini: false,
+      right: null
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#logo {
+  height: 30px;
 }
 
-#logo {
-  width: 300px;
-  border: 0;
+.application--light .toolbar {
+  background-color: #12506B !important; /* match logo */
 }
 </style>
